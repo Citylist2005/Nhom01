73e6c8e25dc8627a2658e2b2c6d5f8ed7f105792
hoanghuy-05 <phamphanhoanghuy2005@gmail.com>
2025-05-17 10:10:03 +0700

    Add files via upload

---
+++ truhaiso.java
@@ -0,0 +1,13 @@
+package hoanghuy;
+import java.util.*;
+public class truhaiso {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		int hieu = a - b;
+		System.out.println("Hieu = " + hieu);
+
+	}
+
+}
