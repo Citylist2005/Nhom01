8e5b648a744b80e618d0882b2abc75cf79a67cdd
hoanghuy-05 <phamphanhoanghuy2005@gmail.com>
2025-05-17 10:23:15 +0700

    Add files via upload

---
+++ giaiptbacnhat.java
@@ -0,0 +1,24 @@
+package hoanghuy;
+import java.util.*;
+public class giaiptbacnhat {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+	        System.out.print("Nhập a: ");
+	        double a = sc.nextDouble();
+	        System.out.print("Nhập b: ");
+	        double b = sc.nextDouble();
+	        if (a == 0) {
+	            if (b == 0) {
+	                System.out.println("Phương trình có vô số nghiệm.");
+	            } else {
+	                System.out.println("Phương trình vô nghiệm.");
+	            }
+	        } else {
+	            double x = -b / a;
+	            System.out.println("Nghiệm của phương trình là: x = " + x);
+	        }
+	        sc.close();
+	}
+
+}
